79cc678076db500e2b1460b322865e50deb8c7a4
jferperez27 <juan_j_fernandez@brown.edu>
2025-07-15 23:11:46 -0500

    basic scrolling implemented, text visualization improved

---
+++ render.py
@@ -0,0 +1,66 @@
+import tkinter
+from browse import URL
+import browse as b
+
+
+WIDTH, HEIGHT = 800, 600
+HSTEP, VSTEP = 13, 18
+SCROLL_STEP = 100
+
+
+class Browser:
+    def __init__(self):
+        self.window = tkinter.Tk()
+        self.canvas = tkinter.Canvas(
+            self.window,
+            width=WIDTH,
+            height=HEIGHT
+        )
+        self.canvas.pack()
+        self.scroll = 0
+        self.window.bind("<Down>", self.scrolldown)
+
+    def load(self, url):
+        body = url.request()
+        text = b.lex(body)
+
+        # self.canvas.create_rectangle(10, 20, 400, 300)
+        # self.canvas.create_oval(100, 100, 150, 150)
+        # self.canvas.create_text(200, 150, text="Hi!")
+        
+        # cursor_x, cursor_y = HSTEP, VSTEP
+        # for c in text:
+        #     self.canvas.create_text(cursor_x, cursor_y, text=c)
+        #     cursor_x += HSTEP
+
+        #     if cursor_x >= WIDTH - HSTEP:
+        #         cursor_y += VSTEP
+        #         cursor_x = HSTEP
+
+        self.display_list = layout(text)
+        self.draw()
+
+    def draw(self):
+        self.canvas.delete("all")
+        for x, y, c in self.display_list:
+            self.canvas.create_text(x, y - self.scroll, text=c)
+
+    def scrolldown(self, e):
+        self.scroll += SCROLL_STEP
+        self.draw()
+
+def layout(text):
+    display_list = []
+    cursor_x, cursor_y = HSTEP, VSTEP
+    for c in text:
+        display_list.append((cursor_x, cursor_y, c))
+        cursor_x += HSTEP
+        if cursor_x >= WIDTH - HSTEP:
+            cursor_y += VSTEP
+            cursor_x = HSTEP
+    return display_list
+
+if __name__ == "__main__":
+    import sys
+    Browser().load(URL(sys.argv[1]))
+    tkinter.mainloop()
